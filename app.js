9307ec1ea689e9cbb2dedd9fd183e7198b60b5e9
Deepanshi Singhal <deepanshisinghal1523@gmail.com>
2024-09-10 20:48:17 +0530

    authentication authorisation part done its time to implement it via project

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 const express = require("express");
 const cookieParser=require("cookie-parser")
-const app=express();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
+const app=express();
 app.use(cookieParser());
 
 app.get("/", (req,res)=>{
-    res.cookie("name", "deepanshi");
-    res.send("done");
+    // res.cookie("name", "deepanshi");
+    // res.send("done");
+    // bcrypt.genSalt(10,function(err,salt){
+    //     bcrypt.hash("Lucifer", salt, function(err, hash){
+    //         console.log(hash);
+    //     })
+    // })
+    let token = jwt.sign({email:"deepanshi@example.com"},"secret");
+    res.cookie("token",token);
+    res.send("done")
 })
 
 app.get("/read", (req,res)=>{
-    console.log(req.cookies);
+    // console.log(req.cookies.token);
+    let data = jwt.verify(req.cookies.token, "secret");
+    console.log(data);
     res.send("read page");
 })
 
